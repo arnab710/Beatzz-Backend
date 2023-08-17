d508f1dd930b6db90c4cb7609dc89aa4ed0e470c
Arnab Dey <arnabdey710@gmail.com>
2023-08-17 14:35:02 +0530

    cookie setting changes

--- Utils/AuthHelperFunctions.js
+++ Utils/AuthHelperFunctions.js
@@ -67,7 +67,7 @@ exports.jwtCookieSetter = (token) =>{
 
             expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
             httpOnly:true,
-            sameSite: 'Strict',
+            sameSite: 'Lax',
             secure : process.env.NODE_ENV === 'production' ? true : false 
         })
     }
