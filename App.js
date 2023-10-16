953556845fe77363fbade4e370f62f398c46a042
Arnab Dey <arnabdey710@gmail.com>
2023-10-16 22:58:23 +0530

    test

--- App.js
+++ App.js
@@ -1,33 +1,38 @@
-const express = require('express');
-const UserRouter = require('./Routes/UserRouter');
-const ProductRouter = require('./Routes/ProductRouter');
-const SecurityMiddleware = require('./Routes/SecurityMiddlewareRouter');
-const ReviewRouter = require('./Routes/ReviewRouter');
-const CartRouter = require('./Routes/CartRouter');
-const OrderRouter = require('./Routes/OrderRotuer');
-const cors = require('cors');
-const PaymentRouter = require('./Routes/PaymentRouter');
+const express = require("express");
+const UserRouter = require("./Routes/UserRouter");
+const ProductRouter = require("./Routes/ProductRouter");
+const SecurityMiddleware = require("./Routes/SecurityMiddlewareRouter");
+const ReviewRouter = require("./Routes/ReviewRouter");
+const CartRouter = require("./Routes/CartRouter");
+const OrderRouter = require("./Routes/OrderRotuer");
+const cors = require("cors");
+const PaymentRouter = require("./Routes/PaymentRouter");
 
 const app = express();
 const api = process.env.API;
 
-
-app.use(cors({
-       origin: process.env.FRONTEND_ORIGIN,
-       credentials: true
-   }));
+app.use(
+	cors({
+		origin: process.env.FRONTEND_ORIGIN,
+		credentials: true,
+	})
+);
 
 app.use(express.json());
 
 //security middleware
 app.use(SecurityMiddleware);
 
+app.get("/", (req, res) => {
+	res.json({ ok });
+});
+
 //path middlewares
-app.use(`/${api}/users`,UserRouter);
-app.use(`/${api}/products`,ProductRouter);
-app.use(`/${api}/reviews`,ReviewRouter);
-app.use(`/${api}/cart`,CartRouter);
-app.use(`/${api}/checkout`,PaymentRouter);
-app.use(`/${api}/orders`,OrderRouter);
+app.use(`/${api}/users`, UserRouter);
+app.use(`/${api}/products`, ProductRouter);
+app.use(`/${api}/reviews`, ReviewRouter);
+app.use(`/${api}/cart`, CartRouter);
+app.use(`/${api}/checkout`, PaymentRouter);
+app.use(`/${api}/orders`, OrderRouter);
 
 module.exports = app;
