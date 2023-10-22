69bafb14f93c1d84a249b9db738f06c1ec75e0e9
Arnab Dey <arnabdey710@gmail.com>
2023-10-22 20:52:04 +0530

    unknown error

--- DatabaseConnection.js
+++ DatabaseConnection.js
@@ -1,15 +1,12 @@
-const mongoose = require('mongoose');
-const DbString = process.env.DB_STRING.replace('<password>',process.env.DB_PASSWORD);
+const mongoose = require("mongoose");
+const DbString = process.env.DB_STRING.replace("<password>", process.env.DB_PASSWORD);
 
-const dbConnection = async()=>{
-
-    try{
-    await mongoose.connect(DbString);
-    console.log("DB Connected");
-    }
-    catch(err){
-        process.exit(1); // Exit process with failure
-    }
-}
+const dbConnection = async () => {
+	try {
+		await mongoose.connect(DbString);
+	} catch (err) {
+		process.exit(1); // Exit process with failure
+	}
+};
 
 module.exports = dbConnection;
