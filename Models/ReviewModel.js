8ae9d2e5774e78cdfa44f01aa616ccde2865ba4a
Arnab Dey <arnabdey710@gmail.com>
2023-10-22 10:51:36 +0530

    some correction

--- Models/ReviewModel.js
+++ Models/ReviewModel.js
@@ -1,34 +1,36 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const ReviewSchema = new mongoose.Schema({
+const ReviewSchema = new mongoose.Schema(
+	{
+		productId: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Product",
+			required: [true, "please provide productId"],
+		},
+		rating: {
+			type: Number,
+			min: [1, "minimum rating should be 1"],
+			max: [5, "minimum rating should be 5"],
+			required: true,
+		},
+		review: {
+			type: String,
+			required: true,
+			minlength: [4, "minimum review length should be 4"],
+			maxlength: [100, "maximum length should be 100"],
+		},
+		userId: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User",
+			required: [true, "Please provide userId"],
+		},
+	},
+	{ timestamps: true }
+);
 
-    productId : {
-        type : mongoose.Schema.Types.ObjectId,
-        ref  : 'Product',
-        required: [true,'please provide productId']
-    },
-    rating: {
-        type : Number,
-        min: [1,'minimum rating should be 1'],
-        max: [5,'minimum rating should be 5'],
-        required : true
-    },
-    review: {
-        type : String,
-        required: true,
-        minlength: [4,'minimum review length should be 4'],
-        maxlength: [100,'maximum length should be 100']
-    },
-    userId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref : 'User',
-        required: [true,'Please provide userId']
-    }
-},{timestamps:true});
+ReviewSchema.index({ productId: 1 });
+ReviewSchema.index({ productId: 1, userId: 1 });
 
-ReviewSchema.index({productId : 1});
-ReviewSchema.index({productId : 1 , userId : 1});
-
-const Review = mongoose.model('Review',ReviewSchema);
+const Review = mongoose.model("Review", ReviewSchema);
 
 module.exports = Review;
--- server.js
+++ server.js
@@ -3,25 +3,23 @@ dotenv.config({ path: `${__dirname}/config.env` });
 
 const dbConnection = require("./DatabaseConnection");
 const app = require("./App");
-const {RedisServer} = require("./RedisConnection");
+const { RedisServer } = require("./RedisConnection");
 
 const server = () => {
-  try {
-    //database connecting
-    dbConnection();
-    //redis Server conecting
-    RedisServer();
+	try {
+		//database connecting
+		dbConnection();
+		//redis Server conecting
+		RedisServer();
 
-
-    const port = process.env.PORT || 8000;
-    //listening to port
-    app.listen(port, () => {
-     if(process.env.NODE_ENV==='development') console.log("listening");
-    });
-
-  } catch (err) {
-    process.exit(1);
-  }
+		const port = process.env.PORT || 8000;
+		//listening to port
+		app.listen(port, () => {
+			if (process.env.NODE_ENV === "development") console.log("listening");
+		});
+	} catch (err) {
+		process.exit(1);
+	}
 };
 //initializing all servers
-server(); 
+server();
