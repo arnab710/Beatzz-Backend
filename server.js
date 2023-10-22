32c174971c1bf7e4abf59c1aa569bd5bd7edfac5
Arnab Dey <arnabdey710@gmail.com>
2023-10-22 20:34:47 +0530

    basic load balancing

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const dotenv = require("dotenv");
 dotenv.config({ path: `${__dirname}/config.env` });
 
+const cluster = require("node:cluster");
+const numCPUs = require("node:os").availableParallelism();
+const process = require("node:process");
 const dbConnection = require("./DatabaseConnection");
 const app = require("./App");
 const { RedisServer } = require("./RedisConnection");
@@ -12,14 +15,25 @@ const server = () => {
 		//redis Server conecting
 		RedisServer();
 
-		const port = process.env.PORT || 8000;
-		//listening to port
-		app.listen(port, () => {
-			if (process.env.NODE_ENV === "development") console.log("listening");
-		});
+		if (cluster.isPrimary) {
+			// Fork workers.
+			for (let i = 0; i < numCPUs; i++) {
+				cluster.fork();
+			}
+
+			cluster.on("exit", (worker, code, signal) => {
+				cluster.fork();
+			});
+		} else {
+			const port = process.env.PORT || 8000;
+			const serverMain = app.listen(port, () => {
+				if (process.env.NODE_ENV === "development") console.log("listening");
+			});
+		}
 	} catch (err) {
 		process.exit(1);
 	}
 };
+
 //initializing all servers
 server();
